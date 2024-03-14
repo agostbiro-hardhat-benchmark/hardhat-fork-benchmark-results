ea79866767da529a9410a12abc7fe3b684ae7dac
github-action-benchmark <github@users.noreply.github.com>
2024-03-14 17:01:14 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for cd0324779b7f6edf5026af5f52475b58e574f202

---
+++ bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1710435674765,
+  "repoUrl": "https://github.com/agostbiro/hardhat-fork",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "agostbiro@gmail.com",
+            "name": "Agost Biro",
+            "username": "agostbiro"
+          },
+          "committer": {
+            "email": "agostbiro@gmail.com",
+            "name": "Agost Biro",
+            "username": "agostbiro"
+          },
+          "distinct": true,
+          "id": "cd0324779b7f6edf5026af5f52475b58e574f202",
+          "message": "Fix",
+          "timestamp": "2024-03-14T17:58:32+01:00",
+          "tree_id": "9c671c9a0523054783f718c1dd26e31672bb7c39",
+          "url": "https://github.com/agostbiro/hardhat-fork/commit/cd0324779b7f6edf5026af5f52475b58e574f202"
+        },
+        "date": 1710435673661,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "seaport_4f4e7c20",
+            "value": 17115.564706,
+            "unit": "ms"
+          },
+          {
+            "name": "All Scenarios",
+            "value": 17115.564706,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
