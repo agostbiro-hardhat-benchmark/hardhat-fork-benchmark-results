3dbcfa0313d476a5ab1dfe68fc3cfcb48640c688
github-action-benchmark <github@users.noreply.github.com>
2024-03-14 18:32:32 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 99e449e3999fc6b83529dd0309bf017ea6ea9a06

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710435674765,
+  "lastUpdate": 1710441152006,
   "repoUrl": "https://github.com/agostbiro/hardhat-fork",
   "entries": {
     "Benchmark": [
@@ -33,6 +33,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "seaport_4f4e7c20",
             "value": 17115.564706,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "5764438+agostbiro@users.noreply.github.com",
+            "name": "Agost Biro",
+            "username": "agostbiro"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "99e449e3999fc6b83529dd0309bf017ea6ea9a06",
+          "message": "Merge pull request #4 from agostbiro/agostbiro/slow\n\nAgostbiro/slow",
+          "timestamp": "2024-03-14T19:27:51+01:00",
+          "tree_id": "4f60ba5e729cb5109ca7a8c5388879b77f20f0e5",
+          "url": "https://github.com/agostbiro/hardhat-fork/commit/99e449e3999fc6b83529dd0309bf017ea6ea9a06"
+        },
+        "date": 1710441150905,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "seaport_4f4e7c20",
+            "value": 17630.946286,
+            "unit": "ms"
+          },
+          {
+            "name": "All Scenarios",
+            "value": 17630.946286,
             "unit": "ms"
           }
         ]
